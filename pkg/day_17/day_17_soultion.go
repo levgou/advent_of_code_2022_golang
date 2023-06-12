ff3a3d4d9f0cd352f1751d706e3accfeff197827
Lev <lev.gourevitch@gmail.com>
2023-06-12 15:16:47 +0300

    Day 17 part 1 works

--- pkg/day_17/day_17_soultion.go
+++ pkg/day_17/day_17_soultion.go
@@ -3,6 +3,7 @@ package day_17
 import (
 	"advent_of_code/pkg/shared"
 	"fmt"
+	"golang.org/x/exp/slices"
 	"log"
 )
 
@@ -11,35 +12,55 @@ const (
 	Demo  = "/Users/stavalaluf/lev_tmp/advent_of_code_2022_golang/pkg/day_17/demo_input.txt"
 )
 
+var Rocks = []rune{'🟥', '🟨', '🟩', '🟦', '🟪'}
+
 const (
-	Rock       = '#'
+	//Rock       = '#'
 	MovingRock = '@'
 	Air        = ' '
 )
 
-var Shapes = [][][]rune{
+type Shape struct {
+	repr  [][]rune
+	color rune
+}
+
+var Shapes = []Shape{
 	{
-		{'@', '@', '@', '@'},
+		[][]rune{{'@', '@', '@', '@'}},
+		'🟥',
 	},
 	{
-		{' ', '@', ' '},
-		{'@', '@', '@'},
-		{' ', '@', ' '},
+		[][]rune{
+			{' ', '@', ' '},
+			{'@', '@', '@'},
+			{' ', '@', ' '},
+		},
+		'🟨',
 	},
 	{
-		{'@', '@', '@'},
-		{'@', ' ', ' '},
-		{'@', ' ', ' '},
+		[][]rune{
+			{'@', '@', '@'},
+			{'@', ' ', ' '},
+			{'@', ' ', ' '},
+		},
+		'🟩',
 	},
 	{
-		{'@'},
-		{'@'},
-		{'@'},
-		{'@'},
+		[][]rune{
+			{'@'},
+			{'@'},
+			{'@'},
+			{'@'},
+		},
+		'🟦',
 	},
 	{
-		{'@', '@'},
-		{'@', '@'},
+		[][]rune{
+			{'@', '@'},
+			{'@', '@'},
+		},
+		'🟪',
 	},
 }
 
@@ -67,10 +88,14 @@ type Point struct {
 	y int
 }
 
+func isRock(char rune) bool {
+	return slices.Index(Rocks, char) != -1
+}
+
 func findHighest() int {
 	for row := len(World) - 1; row >= 0; row-- {
 		for col := 0; col < len(World[row]); col++ {
-			if World[row][col] == Rock {
+			if isRock(World[row][col]) {
 				return row
 			}
 		}
@@ -82,18 +107,22 @@ func findHighest() int {
 func removeShape(shape [][]rune, topLeft Point) {
 	for i, row := range shape {
 		for j, _ := range row {
-			World[topLeft.y+i][topLeft.x+j] = Air
+			if World[topLeft.y+i][topLeft.x+j] == MovingRock {
+				World[topLeft.y+i][topLeft.x+j] = Air
+			}
 		}
 	}
 }
 
-func paintShapeStatic(shape [][]rune, topLeft Point) {
-	for i, row := range shape {
+func paintShapeStatic(shape Shape, topLeft Point) {
+	for i, row := range shape.repr {
 		for j, char := range row {
 			if char == MovingRock {
-				char = Rock
+				char = shape.color
 			}
-			World[topLeft.y+i][topLeft.x+j] = char
+			if char != Air {
+				World[topLeft.y+i][topLeft.x+j] = char
+			}
 		}
 	}
 }
@@ -101,7 +130,9 @@ func paintShapeStatic(shape [][]rune, topLeft Point) {
 func paintShape(shape [][]rune, topLeft Point) {
 	for i, row := range shape {
 		for j, char := range row {
-			World[topLeft.y+i][topLeft.x+j] = char
+			if char != Air {
+				World[topLeft.y+i][topLeft.x+j] = char
+			}
 		}
 	}
 }
@@ -109,7 +140,7 @@ func paintShape(shape [][]rune, topLeft Point) {
 func shapeTouchesOther(shape [][]rune, topLeft Point) bool {
 	for i, row := range shape {
 		for j, char := range row {
-			if char == MovingRock && World[topLeft.y+i][topLeft.x+j] == Rock {
+			if char == MovingRock && isRock(World[topLeft.y+i][topLeft.x+j]) {
 				return true
 			}
 		}
@@ -160,32 +191,32 @@ func run(jets string, rockCount int) {
 		shape := Shapes[curShape]
 
 		//shapeHeight := len(shape)
-		shapeWidth := len(shape[0])
+		shapeWidth := len(shape.repr[0])
 
 		shapeTopLeft := Point{
 			Width - shapeWidth - StartLeftDist,
 			highest + StartBottomDist,
 		}
 
-		println("\n---- NEW")
-		paintShape(shape, shapeTopLeft)
-		printWorld()
+		//println("\n---- NEW")
+		paintShape(shape.repr, shapeTopLeft)
+		//printWorld()
 
 		for {
 			jet := jets[curJet]
 			//fmt.Printf("jet >>>>>>>>> [%d] [%c] \n", curJet, rune(jet))
 
-			afterHoriz := moveShapeHoriz(shape, shapeTopLeft, rune(jet))
+			afterHoriz := moveShapeHoriz(shape.repr, shapeTopLeft, rune(jet))
 			curJet = (curJet + 1) % len(jets)
-			afterDown := moveShapeDown(shape, afterHoriz)
+			afterDown := moveShapeDown(shape.repr, afterHoriz)
 
-			removeShape(shape, shapeTopLeft)
-			paintShape(shape, afterDown)
+			removeShape(shape.repr, shapeTopLeft)
+			paintShape(shape.repr, afterDown)
 
 			if afterDown == afterHoriz {
 				paintShapeStatic(shape, afterDown)
 				highest = findHighest()
-				fmt.Println("highest", highest)
+				//fmt.Println("highest", highest)
 				break
 
 			} else {
@@ -197,34 +228,60 @@ func run(jets string, rockCount int) {
 				World = append(World, Line)
 			}
 		}
-		printWorld()
+		//printWorld()
 		curShape = (curShape + 1) % len(Shapes)
 	}
 }
 
 func printWorld() {
-	println("\n     + - - - - - - - +")
+
+	for i := len(World) - 1; i >= 0; i-- {
+		fmt.Printf("%05d| ", i)
+		for j := Width - 1; j >= 0; j-- {
+			char := World[i][j]
+			if char == ' ' {
+				char = '.'
+			} else if char == MovingRock {
+				char = '💠'
+			}
+			print(string(char))
+			if char == '.' {
+				print(" ")
+			}
+		}
+		println("|")
+	}
+
+	println("     + - - - - - - - +\n")
+
+	return
+
 	for i, row := range World {
 		fmt.Printf("%05d| ", i)
 		for _, char := range row {
 			if char == ' ' {
 				char = '.'
+			} else if char == MovingRock {
+				char = '💠'
 			}
-			print(string(char) + " ")
+			print(string(char))
+			if char == '.' {
+				print(" ")
+			}
 		}
 		println("|")
 	}
 }
 
 func Solution() {
-	lines, err := shared.ReadLines(Demo)
+	lines, err := shared.ReadLines(Input)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
 
 	jets := lines[0]
-	run(jets, 11)
+	run(jets, 2022)
 
 	//printWorld()
 }
